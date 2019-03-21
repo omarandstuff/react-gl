31cd2f980e06c2ea8ab370f1a8340c07b3ea5cd4
David De Anda <omarandstuff@gmail.com>
2019-03-20 18:24:26 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Ad3D from "./components/Ad3D";
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="main-content">
+          <h1>Unotalks</h1>
+          <h2>Organized by Andrea Montelongo</h2>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Surprise steepest recurred landlord mr wandered amounted of.
+            Continuing devonshire but considered its. Rose past oh shew roof is
+            song neat. Do depend better praise do friend garden an wonder to.
+            Intention age nay otherwise but breakfast. Around garden beyond to
+            extent by. Bed sincerity yet therefore forfeited his certainty
+            neglected questions. Pursuit chamber as elderly amongst on. Distant
+            however warrant farther to of. My justice wishing prudent waiting in
+            be. Comparison age not pianoforte increasing delightful now.
+            Insipidity sufficient dispatched any reasonably led ask. Announcing
+            if attachment resolution sentiments admiration me on diminution.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Ad3D />
+          <p>
+            Mr do raising article general norland my hastily. Its companions say
+            uncommonly pianoforte favourable. Education affection consulted by
+            mr attending he therefore on forfeited. High way more far feet kind
+            evil play led. Sometimes furnished collected add for resources
+            attention. Norland an by minuter enquire it general on towards
+            forming. Adapted mrs totally company two yet conduct men. So
+            delightful up dissimilar by unreserved it connection frequently. Do
+            an high room so in paid. Up on cousin ye dinner should in. Sex stood
+            tried walls manor truth shy and three his. Their to years so child
+            truth. Honoured peculiar families sensible up likewise by on in.
+            Ever man are put down his very. And marry may table him avoid. Hard
+            sell it were into it upon. He forbade affixed parties of assured to
+            me windows. Happiness him nor she disposing provision. Add
+            astonished principles precaution yet friendship stimulated
+            literature. State thing might stand one his plate. Offending or
+            extremity therefore so difficult he on provision. Tended depart
+            turned not are. Silent sir say desire fat him letter. Whatever
+            settling goodness too and honoured she building answered her.
+            Strongly thoughts remember mr to do consider debating. Spirits
+            musical behaved on we he farther letters. Repulsive he he as
+            deficient newspaper dashwoods we. Discovered her his pianoforte
+            insipidity entreaties. Began he at terms meant as fancy. Breakfast
+            arranging he if furniture we described on. Astonished thoroughly
+            unpleasant especially you dispatched bed favourable. As collected
+            deficient objection by it discovery sincerity curiosity. Quiet decay
+            who round three world whole has mrs man. Built the china there tried
+            jokes which gay why. Assure in adieus wicket it is. But spoke round
+            point and one joy. Offending her moonlight men sweetness see
+            unwilling. Often of it tears whole oh balls share an. Now eldest new
+            tastes plenty mother called misery get. Longer excuse for county nor
+            except met its things. Narrow enough sex moment desire are. Hold who
+            what come that seen read age its. Contained or estimable earnestly
+            so perceived. Imprudence he in sufficient cultivated. Delighted
+            promotion improving acuteness an newspaper offending he. Misery in
+            am secure theirs giving an. Design on longer thrown oppose am. He
+            difficult contented we determine ourselves me am earnestly. Hour no
+            find it park. Eat welcomed any husbands moderate. Led was misery
+            played waited almost cousin living. Of intention contained is by
+            middleton am. Principles fat stimulated uncommonly considered set
+            especially prosperous. Sons at park mr meet as fact like. Friendship
+            contrasted solicitude insipidity in introduced literature it. He
+            seemed denote except as oppose do spring my. Between any may mention
+            evening age shortly can ability regular. He shortly sixteen of
+            colonel colonel evening cordial to. Although jointure an my of
+            mistress servants am weddings. Age why the therefore education
+            unfeeling for arranging. Above again money own scale maids ham least
+            led. Returned settling produced strongly ecstatic use yourself way.
+            Repulsive extremity enjoyment she perceived nor.
+          </p>
+        </div>
       </div>
     );
   }
---
+++ src/components/Ad3D.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "babylonjs-loaders";
+import * as BABYLON from "babylonjs";
+
+import "./Ad3D.css";
+import model from "../models/unotalks.obj";
+import envTexture from "../textures/env.hdr";
+import diffuseTexture from "../textures/diffuse.png";
+import reflectionTexture from "../textures/reflect.png";
+
+const RAD_DEGREE_FACTOR = 0.0174533;
+
+export default class Ad3D extends React.Component {
+  static propTypes = {};
+
+  state = {};
+
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const canvas = this.canvasRef.current;
+
+    this.canvasRef.current.onwheel = function(event){
+      event.preventDefault()
+    }
+
+    this.canvasRef.current.onmousewheel = function(event){
+      event.preventDefault()
+    }
+
+    var engine = new BABYLON.Engine(canvas, true, {
+      preserveDrawingBuffer: true,
+      stencil: true
+    });
+
+    var createScene = function() {
+      var scene = new BABYLON.Scene(engine);
+      scene.clearColor = new BABYLON.Color4(1, 1, 1, 0.0);
+
+      var camera = new BABYLON.ArcRotateCamera(
+        "camera1",
+        RAD_DEGREE_FACTOR * -90,
+        RAD_DEGREE_FACTOR * 90,
+        6,
+        BABYLON.Vector3.Zero(),
+        scene
+      );
+
+      camera.attachControl(canvas, false, true);
+
+      var light = new BABYLON.HemisphericLight(
+        "light1",
+        new BABYLON.Vector3(-1, 1, -1),
+        scene
+      );
+      var light2 = new BABYLON.HemisphericLight(
+        "light2",
+        new BABYLON.Vector3(1, 1, 1),
+        scene
+      );
+
+      light.intensity = 7;
+      light2.intensity = 2;
+
+      BABYLON.SceneLoader.ImportMesh(null, model, "", scene, function(
+        meshes,
+        particleSystems,
+        skeletons
+      ) {
+        var pbr = new BABYLON.PBRMetallicRoughnessMaterial("pbr", scene);
+        pbr.baseTexture = new BABYLON.Texture(diffuseTexture, scene);
+        pbr.metallic = 1;
+        pbr.roughness = 1;
+        pbr.environmentTexture = new BABYLON.HDRCubeTexture(
+          envTexture,
+          scene,
+          64,
+          true
+        );
+        pbr.environmentTexture.coordinatesMode = BABYLON.Texture.LINEAR_LINEAR;
+        pbr.metallicRoughnessTexture = new BABYLON.Texture(
+          reflectionTexture,
+          scene
+        );
+
+        meshes[0].material = pbr;
+      });
+
+      return scene;
+    };
+
+    var scene = createScene();
+
+    engine.runRenderLoop(function() {
+      scene.render();
+    });
+
+    window.addEventListener("resize", function() {
+      engine.resize();
+    });
+  }
+
+  render() {
+    return (
+      <div className="ad-3d">
+        <canvas ref={this.canvasRef} id="renderCanvas" />
+      </div>
+    );
+  }
+}
